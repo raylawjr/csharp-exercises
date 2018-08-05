1f3578ec2e58ab3500f763487c2a1f3f625ecabc
raylawjr <raylawjr@gmail.com>
2018-08-05 00:14:59 -0500

    finished Studio 1

---
+++ Area/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string radiusstr;
+            double radiusdbl;
+            double area;
+            Console.WriteLine("Hello, I will find the area of a circle for you. Please enter a radius (using digits).  Your radius must be a positive number!");
+            radiusstr = Console.ReadLine();
+            bool radiuscheck = double.TryParse(radiusstr, out radiusdbl);
+            do
+            {
+                Console.WriteLine("You didn't enter a valid radius!");
+                Console.ReadLine();
+                Console.WriteLine("Please enter a radius (using digits).");
+                radiusstr = Console.ReadLine();
+                radiuscheck = double.TryParse(radiusstr, out radiusdbl);
+            } while ((radiuscheck == false) || (radiusdbl <= 0));
+                area = 3.14 * (radiusdbl * radiusdbl);
+                Console.WriteLine("The area of a circle with radius {0} is {1}.", radiusdbl, area);
+                Console.ReadLine();
+        }
+    }
+}
