1f1576b351265b69c25a7e71589a405f94a23f3d
raylawjr <raylawjr@gmail.com>
2018-08-07 22:09:43 -0500

    finished class 2 prep exercises

---
+++ Class2PrepStringBuilder/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradebookDict
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+            do
+            {
+                Console.Write("name: ");
+                newStudent = Console.ReadLine();
+                if (newStudent != "")
+                {
+                    // Get the student's grade
+                    Console.WriteLine("grade: ");
+                    double newGrade = double.Parse(Console.ReadLine());
+
+                    students.Add(newStudent, newGrade);
+                }
+            }
+            while (newStudent != "");
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+            foreach (KeyValuePair<string, double> student in students)
+            {
+                StringBuilder sb = new StringBuilder(student.Key);
+                sb.Append(" (" + student.Value + ")");
+                Console.WriteLine(sb);
+            }
+
+            double sum = students.Values.Sum();
+            double avg = sum / students.Count;
+            Console.WriteLine("Average grade: " + avg);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class2PrepStudentID/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class2PrepStudentID
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudent;
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+            do
+            {
+                Console.WriteLine("name: ");
+                newStudent = Console.ReadLine();
+                if (newStudent != "")
+                {
+                    Console.WriteLine("ID: ");
+                    int newID = int.Parse(Console.ReadLine());
+
+                    students.Add(newID, newStudent);
+                }
+            }
+            while (newStudent != "");
+
+            Console.WriteLine("\nClass roster:");
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Key + " (" + student.Value + ")");
+            }
+            Console.ReadLine();
+        }
+    }
+}
